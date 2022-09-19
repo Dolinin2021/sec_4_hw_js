9e4fb6f550c52b1c492433f010a8b10c6a7c71f7
Dolinin2021 <programmistdolinin@mail.ru>
2022-09-19 19:42:47 +0600

    Fix

--- task_1_sec_4.js
+++ task_1_sec_4.js
@@ -4,5 +4,18 @@ function getPasswordChecker(password){
     }
 }
 
+//true
 c = getPasswordChecker("1945");
 console.log(c("1945"));
+
+//false
+c = getPasswordChecker("1946");
+console.log(c("1945"));
+
+//false
+c = getPasswordChecker("room");
+console.log(c("class"));
+
+//true
+c = getPasswordChecker("room");
+console.log(c("room"));
